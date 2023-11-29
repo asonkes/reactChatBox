a2aebbe5b02ea4c62c3de9ded4db1613a1347ebc
asonkes <audrey.sonkes@gmail.com>
2023-11-29 11:32:27 +0100

    changement fichier app.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import './App.css';
 import Formulaire from './components/Formulaire';
 import Message from './components/Message';
 
-
-class App extends Component {
-  state = {  } 
-  render() { 
-    return (
-      <div className='box'>
-        <div>
-          <div className='messages'>
-             <Message />
-             <Message />
-             <Message />
-          </div>
-        </div>
-        <Formulaire />
-      </div>
-    );
-  }
+const App = () => {
+return ( 
+  <div className="box">
+  <div>
+    <div className="messages">
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  </div>
+  <Formulaire />
+</div>
+ );
 }
- 
+
 export default App;
