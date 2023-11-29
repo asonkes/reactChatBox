5edd4d64c946a988fc09fcda635b5ddd556ed6db
asonkes <audrey.sonkes@gmail.com>
2023-11-29 12:46:49 +0100

    chatBox - gestion des messages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
+// On change pour avoir un "State"
+import React, { useState } from 'react';
 import './App.css';
 import Formulaire from './components/Formulaire';
 import Message from './components/Message';
+import { useParams } from 'react-router-dom';
 
 const App = () => {
+  let {login} = useParams()
+
+  // Mon state il s'appelle "pseudo"
+  // On a une méthode propre à chaque "state"
+  const [pseudo, setPseudo] = useState(login)
+  const [messages, setMessages] = useState({})
+
+  // Lui , c'est l'ensemble des messages.
+  //const [message, setMessages] = useState({})
+
+  // ajout des messages aux autres messages(boit avec tous les messages)
+  const addMessage = message => {
+    const newMessages = {...messages}
+
+    // [1] ==> correspond à l'index
+    //newMessages[1] = message ==> On change cette methode à celle en -dessous
+    // Cette méthode permet de pouvoir à la limite utiliser et mettre en place la date d'envoi du message...
+    newMessages[`message-${Date.now()}`] = message
+
+    // set = car je modifie ma barrière d'état
+    setMessages({newMessages})
+  }
+
 return ( 
   <div className="box">
   <div>
@@ -13,7 +39,10 @@ return (
       <Message />
     </div>
   </div>
-  <Formulaire />
+  <Formulaire 
+  pseudo={pseudo}
+  addMessage={addMessage}
+  />
 </div>
  );
 }
--- src/components/Formulaire.jsx
+++ src/components/Formulaire.jsx
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
 
 class Formulaire extends Component {
-    state = {  } 
+    state = { 
+        message: ''
+     } 
+
+     createMessage = () => {
+        const {pseudo, addMessage} = this.props
+
+        // Message que l'utilisateur envoi à Dyma pour qu'il réponde.
+        const message = {
+            pseudo: pseudo,
+            message: this.state.message
+        }
+
+        addMessage(message)
+
+        //reset
+        this.setState({message: ''})
+     }
+
+     // Grâce à cette partie, on peut écrire, dans le bloc qui ets le formulaire....
+     handleChange = event => {
+        const message = event.target.value 
+        this.setState({message:message})
+     }
+
+     // Permet de voir si le texte (formulaire) ets bien envoyé...
+     // Aller voir dans Components/hooks/ pour voir si message bien envoyé !!!
+     handleSubmit = event => {
+        event.preventDefault()
+        this.createMessage()
+     }
+
     render() { 
         return (
-            <form className='form'>
-                <textarea 
-                required
-                maxlength='140'>
-                </textarea>
+            <form className='form' onSubmit={this.handleSubmit}>
+                
+                <textarea
+                    value={this.state.message}
+                    onChange={this.handleChange} 
+                    required
+                    maxLength='140' 
+                />
+
+                <div className='info'>
+                    140
+                </div>
+
                 <button type="submit">
                     Envoyer
                 </button>
